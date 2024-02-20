b375eec4f6f414ee6aed74f6bedd1db068d3694d
adnanahad <adnanahad.in@gmail.com>
2024-02-20 10:10:52 +0530

    added button component for coloring the buttons below the places

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -5,7 +5,7 @@ function Users() {
   console.log("I am iin users");
   const USERS = [
     {
-      id: "U1",
+      id: "u1",
       name: "Max Shewan",
       image:
         "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
