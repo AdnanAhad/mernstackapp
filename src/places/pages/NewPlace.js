89db7691dad2f51af435ee058db8867ba7a128bf
adnanahad <adnanahad.in@gmail.com>
2024-02-24 11:23:51 +0530

    added useCallback function to reduce re-renders

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 
 function NewPlace() {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title  "
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
-        // onInput={}
+        errorText="please enter a valid description (at least 5 caharacters)"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="please enter a valid input"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
